8e4b4d3201561bc396a41c122fb0bcd0c59cf868
Tri Fennia Lesmana <sg.shipshop@gmail.com>
2021-11-28 18:04:59 +0700

    change to general item calculator

--- js/interaction.js
+++ js/interaction.js
@@ -1,14 +1,163 @@
 $(document).ready(function(){
-    // open modal
-    // $(".").click(function(){
+    function formatRupiah (num){
+        num_string = num.toString(),
+        sisa 	= num_string.length % 3,
+        rupiah 	= num_string.substr(0, sisa),
+        ribuan 	= num_string.substr(sisa).match(/\d{3}/g);
+            
+        if (ribuan) {
+            separator = sisa ? '.' : '';
+            rupiah += separator + ribuan.join('.');
+        }
+        return "Rp" + rupiah;
+    }
 
-    // });
-
-    // close modal
-    $(".result_close").click(function(){
-        $(".result").fadeOut();
+    // get SGD to IDR rate
+    fetch("https://currency-exchange.p.rapidapi.com/exchange?from=SGD&to=IDR&q=1.0", {
+        "method": "GET",
+        "headers": {
+            "x-rapidapi-host": "currency-exchange.p.rapidapi.com",
+            "x-rapidapi-key": "b0fcb07fd8mshe7bb5932c2718a3p1c07a0jsnd1e84edd8c4e"
+        }
+    })
+    .then(response => {
+        response.json().then( res => {
+            rate_sgdidr = Math.ceil(res*1.05 / 100) * 100;
+            document.getElementById("rate_sgdidr").innerHTML = formatRupiah(rate_sgdidr);
+        })
+        .catch(err => {
+            console.error(err);    
+        })
+    })
+    .catch(err => {
+        console.error(err);
     });
 
+    // get USD to IDR rate
+    fetch("https://currency-exchange.p.rapidapi.com/exchange?from=USD&to=IDR&q=1.0", {
+        "method": "GET",
+        "headers": {
+            "x-rapidapi-host": "currency-exchange.p.rapidapi.com",
+            "x-rapidapi-key": "b0fcb07fd8mshe7bb5932c2718a3p1c07a0jsnd1e84edd8c4e"
+        }
+    })
+    .then(response => {
+        response.json().then( res => {
+            rate_usdidr = Math.ceil(res / 100) * 100;
+            document.getElementById("rate_usdidr").innerHTML = formatRupiah(rate_usdidr);
+        })
+        .catch(err => {
+            console.error(err);    
+        })
+    })
+    .catch(err => {
+        console.error(err);
+    });
 
+    $('#successAlert').hide();
+    // $('#alert').alert('close');
+    // copy clipboard
+    const txt = document.querySelector('#detailHarga');
+    const btn = document.querySelector('#copyBtn');
 
+    const copy = (text) => {
+        const textarea = document.createElement('textarea');
+        document.body.appendChild(textarea);
+        textarea.value = text;
+        textarea.select();
+        document.execCommand('copy');
+        textarea.remove();
+    }
+
+    btn.addEventListener('click', (e) => {
+        if (txt.value != ""){
+            copy(txt.value);
+            $('#successAlert').show();
+            setTimeout(function () {
+                $('#successAlert').hide();
+            }, 2000);
+        }
+    });
+
+    $("#generateHarga").click(function(){
+        selectedJenisBarangId = document.getElementById("jenisBarang").value;
+        hargaBarang = document.getElementById("hargaBarang").value;
+
+        beratTimbangan = document.getElementById("beratTimbangan").value;
+        beratVolumetrik = 0;
+        p = document.getElementById("dimensiPaketP").value;
+        l = document.getElementById("dimensiPaketL").value;
+        t = document.getElementById("dimensiPaketT").value;
+        beratDigunakan = 0;
+
+        // dapetin ongkir
+        ongkirObj = {};
+        for (i = 0; i < ongkir.length; i++) {
+            if (selectedJenisBarangId == ongkir[i]["id"]) {
+                ongkirObj = ongkir[i];
+                break;
+            }
+        }
+        // console.log(ongkirObj);
+
+        // hitung harga
+        if (isNaN(hargaBarang) || hargaBarang == "" || hargaBarang <= 0) {
+            hargaBarang = 0;
+        }
+        hargaBarang = hargaBarang * rate_sgdidr;
+
+        // ongkos jastip
+        ongkosJastip = 0;
+        if (hargaBarang < 1500000) {
+            ongkosJastip = 200000;
+        }
+        else if(hargaBarang < 2500000) {
+            ongkosJastip = 350000;
+        }
+        else {
+            ongkosJastip = 500000;
+        }
+
+        // hitung berat
+        // berat timbangan
+        if (isNaN(beratTimbangan) || beratTimbangan == "" || beratTimbangan <= 0) {
+            beratTimbangan = 0;
+        }
+
+        // berat volumetrik
+        if (!isNaN(p) & !isNaN(l) && !isNaN(t) &&
+            p != "" && l != "" || t != "" &&
+            p > 0 && l > 0 && t > 0) {
+            beratVolumetrik = p * l * t / 6000
+        }
+        else{
+            beratVolumetrik = 0;
+        }
+
+        beratDigunakan = (beratTimbangan > beratVolumetrik) ? beratTimbangan : beratVolumetrik;
+        beratDigunakan = Math.round(beratDigunakan);
+
+        totalOngkir = beratDigunakan * ongkirObj["ongkir_per_kg"];
+
+        if (ongkirObj["threshold_additional_charge"]) {
+            // if exist
+            if (hargaBarang > ongkirObj["threshold_additional_charge"]) {
+                totalOngkir += ongkirObj["additional_charge"];
+            }
+        }
+
+        text = "berikut rinciannya kak :)" +
+                "\nharga barang = " + formatRupiah(hargaBarang) +
+                "\nongkos jastip = " + formatRupiah(ongkosJastip) +
+                "\nongkos kirim (estimasi " + beratDigunakan + "kg) = " + formatRupiah(totalOngkir) +
+                "\nTOTAL = " + formatRupiah(hargaBarang + ongkosJastip + totalOngkir);
+
+        txt.value = text;
+
+        var el = txt;
+        setTimeout(function(){
+          el.style.cssText = 'height:auto; padding:0';
+          el.style.cssText = 'height:' + el.scrollHeight + 'px';
+        },0);
+    });
 });
---
+++ js/ongkir.js
@@ -0,0 +1,58 @@
+ongkir = [
+    {
+        "id": 1,
+        "jenis_barang": "general item 300rb per kg",
+        "ongkir_per_kg": 300000,
+    },
+    {
+        "id": 2,
+        "jenis_barang": "sendal, sepatu",
+        "ongkir_per_kg": 300000,
+        "threshold_additional_charge": 150000,
+        "additional_charge": 200000
+    },
+    {
+        "id": 3,
+        "jenis_barang": "tas, dompet",
+        "ongkir_per_kg": 350000,
+        "threshold_additional_charge": 15000000,
+        "additional_charge": 1000000
+    },
+    {
+        "id": 4,
+        "jenis_barang": "aksesoris branded",
+        "ongkir_per_kg": 400000,
+        "threshold_additional_charge": 3000000,
+        "additional_charge": 1000000
+    },
+    {
+        "id": 5,
+        "jenis_barang": "elektronik: speaker, airpods, earphone, headphone, kamera, gopro",
+        "ongkir_per_kg": 350000,
+    },
+    {
+        "id": 6,
+        "jenis_barang": "hp, ipad, jam tangan",
+        "ongkir_per_kg": 1600000,
+    },
+    {
+        "id": 7,
+        "jenis_barang": "laptop (max 3kg)",
+        "ongkir_per_kg": 2200000,
+    },
+    {
+        "id": 8,
+        "jenis_barang": "apple watch",
+        "ongkir_per_kg": 1300000,
+    },
+    {
+        "id": 9,
+        "jenis_barang": "sepeda, frame sepeda",
+        "ongkir_per_kg": 400000,
+    },
+    {
+        "id": 10,
+        "jenis_barang": "spare part",
+        "ongkir_per_kg": 350000,
+    },
+]
