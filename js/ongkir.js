16aca4ad653eb8451d1ae7ac2b3eb8936da4aeac
Tri Fennia Lesmana <sg.shipshop@gmail.com>
2021-11-29 08:49:34 +0700

    fix ongkir

--- js/ongkir.js
+++ js/ongkir.js
@@ -1,56 +1,61 @@
 ongkir = [
     {
         "id": 1,
-        "jenis_barang": "general item 300rb per kg",
+        "jenis_barang": "(s) general item 300rb per kg",
         "ongkir_per_kg": 300000
     },
     {
         "id": 2,
-        "jenis_barang": "sendal, sepatu",
+        "jenis_barang": "(s) sendal, sepatu",
         "ongkir_per_kg": 300000,
         "threshold_additional_charge": 1500000,
         "additional_charge": 200000
     },
     {
         "id": 3,
-        "jenis_barang": "tas, dompet",
+        "jenis_barang": "(s) tas, dompet",
         "ongkir_per_kg": 350000,
         "threshold_additional_charge": 15000000,
         "additional_charge": 1000000
     },
     {
         "id": 4,
-        "jenis_barang": "aksesoris branded (harga >3jt)",
+        "jenis_barang": "(s) aksesoris branded (harga >3jt)",
         "ongkir_per_kg": 400000
     },
     {
         "id": 5,
-        "jenis_barang": "elektronik: speaker, airpods, earphone, headphone, kamera, gopro",
+        "jenis_barang": "(s) elektronik: speaker, airpods, earphone, headphone, kamera, gopro",
         "ongkir_per_kg": 350000
     },
     {
         "id": 6,
-        "jenis_barang": "hp, ipad, jam tangan",
+        "jenis_barang": "(s) hp, ipad, jam tangan",
         "ongkir_per_kg": 1600000
     },
     {
         "id": 7,
-        "jenis_barang": "laptop (max 3kg)",
+        "jenis_barang": "(s) laptop (max 3kg)",
         "ongkir_per_kg": 2200000
     },
     {
         "id": 8,
-        "jenis_barang": "apple watch",
+        "jenis_barang": "(s) apple watch",
         "ongkir_per_kg": 1300000
     },
     {
         "id": 9,
-        "jenis_barang": "sepeda, frame sepeda",
+        "jenis_barang": "(s) sepeda, frame sepeda",
         "ongkir_per_kg": 400000
     },
     {
         "id": 10,
-        "jenis_barang": "spare part",
+        "jenis_barang": "(s) spare part",
         "ongkir_per_kg": 350000
+    },
+    {
+        "id": 11,
+        "jenis_barang": "(b) sepatu",
+        "ongkir_per_kg": 250000
     }
 ]
